ad7e53e4c049a2f534bf80e37fe85b4602b5be8d
Drake Ballard <drakeballard@gmail.com>
2016-10-22 15:14:32 -0500

    adding the file structure

---
+++ main.js
@@ -0,0 +1,2 @@
+// will contain the logic of your app. Running it in Terminal/Bash will start the game.
+// The app should end when a player guesses the correct word or runs out of guess
---
+++ word.js
@@ -0,0 +1,2 @@
+// constructor file
+// should contain all of the methods which will check the letters guessed versus the random word selected.
